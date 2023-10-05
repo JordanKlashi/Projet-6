52bcc1d0101c0efbbdbf4b698b91b03dbc804e54
Verbreuk Jordan <jordan-010@hotmail.com>
2023-10-05 20:53:48 +0200

    premier jet
    mise en place de la génération des projets
    créer des filtres
    commencement de la mise en place des compte users

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/login.js
@@ -0,0 +1,15 @@
+const loginMail = document.querySelector(".loginmail")
+
+const loginPassword = document.querySelector(".loginpassword")
+
+const loginSubmit = document.querySelector(".loginsubmit")
+
+loginSubmit.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (loginPassword.value="S0phie"){
+        if (loginMail.value="sophie.bluel@test.tld"){
+            console.log("ça marche bien")
+        }}
+    
+    
+})
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,1 +1,83 @@
-fetch("http://localhost:5678/api-docs/#/default/post_users_login");
+
+const apiWorks = "http://localhost:5678/api/works"
+
+// on récupére données de l'API
+
+fetch(apiWorks)
+    .then(reponse => {
+        if (!reponse.ok) {
+            throw new Error(`Erreur HTTP! statut: ${reponse.status}`)
+        }
+        return reponse.json();
+    })
+    .then(data => {
+        projets = data
+        mesProjets(projets)
+
+    })
+    .catch(error => {
+        console.error("Une erreur s'est produite lors de la récupération des données", error);
+
+    })
+
+// on génére les projets
+const gallery = document.querySelector(".gallery")
+
+function mesProjets(projets) {
+    gallery.innerHTML = ""
+    for (let i = 0; i < projets.length; i++) {
+        
+        const article = projets[i];
+        const projet = document.createElement("figure")
+        const imageProjet = document.createElement("img")
+        imageProjet.src = article.imageUrl;
+        imageProjet.alt = article.title;
+        const textProjet = document.createElement("figcaption")
+        textProjet.innerText = article.title
+        
+        // On rattache les balises a la section gallery
+
+        gallery.appendChild(projet);
+        projet.appendChild(imageProjet);
+        projet.appendChild(textProjet);
+
+    }
+}
+
+
+
+
+
+//gestion des bouttons
+const boutonTous = document.getElementById("tous")
+boutonTous.addEventListener("click", function () {
+    
+    mesProjets(projets)
+})
+
+
+
+const boutonObjets = document.querySelector(".objets")
+boutonObjets.addEventListener('click', () => {
+   
+    const projetObjets = projets.filter(function (projets){
+        return projets.categoryId === 1
+    })
+    mesProjets(projetObjets)
+})
+
+const boutonAppartements = document.querySelector(".appartements")
+boutonAppartements.addEventListener("click", () => {
+    
+    const projetAppartements = projets.filter(function (projets){
+        return projets.categoryId === 2
+    })
+    mesProjets(projetAppartements)
+})
+
+const boutonHotelRestaurant = document.querySelector(".hotel-restaurants")
+boutonHotelRestaurant.addEventListener("click", () => {
+    
+    const projetHotelRestaurant = projets.filter(projets => projets.categoryId === 3)
+    mesProjets(projetHotelRestaurant)
+})
