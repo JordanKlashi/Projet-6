8750abb821c280a08876b5c39c53c207a4621b30
Verbreuk Jordan <jordan-010@hotmail.com>
2023-10-08 11:17:34 +0200

    push
    push

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,7 +1,12 @@
 
 const apiWorks = "http://localhost:5678/api/works"
+const apiCategory = "http://localhost:5678/api/categories"
 
-// on récupére données de l'API
+
+let projets = []
+let category = 0
+let categoryData = []
+// on récupére données de l'API Works
 
 fetch(apiWorks)
     .then(reponse => {
@@ -12,7 +17,7 @@ fetch(apiWorks)
     })
     .then(data => {
         projets = data
-        mesProjets(projets)
+        mesProjets()
 
     })
     .catch(error => {
@@ -20,64 +25,141 @@ fetch(apiWorks)
 
     })
 
+
+// on récupére données de l'API Categories
+
+fetch(apiCategory)
+    .then(reponse => {
+        if (!reponse.ok) {
+            throw new Error(`Erreur HTTP! statut: ${reponse.status}`)
+        }
+        return reponse.json();
+    })
+    .then(data => {
+        categoryData = data
+        createButton()
+
+    })
+    .catch(error => {
+        console.error("Une erreur s'est produite lors de la récupération des données", error);
+
+    })
+
+
 // on génére les projets
 const gallery = document.querySelector(".gallery")
 
-function mesProjets(projets) {
+function mesProjets() {
     gallery.innerHTML = ""
     for (let i = 0; i < projets.length; i++) {
-        
-        const article = projets[i];
-        const projet = document.createElement("figure")
-        const imageProjet = document.createElement("img")
-        imageProjet.src = article.imageUrl;
-        imageProjet.alt = article.title;
-        const textProjet = document.createElement("figcaption")
-        textProjet.innerText = article.title
-        
-        // On rattache les balises a la section gallery
+        if (category === projets[i].categoryId || category === 0) {
+            const article = projets[i];
+            const projet = document.createElement("figure")
+            const imageProjet = document.createElement("img")
+            imageProjet.src = article.imageUrl;
+            imageProjet.alt = article.title;
+            const textProjet = document.createElement("figcaption")
+            textProjet.innerText = article.title
 
-        gallery.appendChild(projet);
-        projet.appendChild(imageProjet);
-        projet.appendChild(textProjet);
+            // On rattache les balises a la section gallery
 
+            gallery.appendChild(projet);
+            projet.appendChild(imageProjet);
+            projet.appendChild(textProjet);
+
+        }
     }
 }
 
+// on génére les boutons
+const divFiltres = document.querySelector(".filtres")
 
+function createButton() {
+    for (let index = 0; index < categoryData.length; index++) {
+        const bouton = categoryData[index]
+        const boutonFiltres = document.createElement('button')
+        boutonFiltres.classList.add("filtres-boutton")
 
+        // on génére le nom des boutons 
 
+        boutonFiltres.innerText = `${bouton.name}`
 
-//gestion des bouttons
+        // on ajoute un EventListener sur la boucle des boutons
+
+        boutonFiltres.addEventListener("click", () => {
+            console.log("ça marche")
+            category = bouton.id
+            mesProjets()
+        })
+
+        // On rattache les balises a la section filtres
+
+        divFiltres.appendChild(boutonFiltres)
+        console.log(bouton)
+    }
+}
+
+//gestion du boutton tous 
+
 const boutonTous = document.getElementById("tous")
 boutonTous.addEventListener("click", function () {
-    
-    mesProjets(projets)
+    category = 0;
+    mesProjets()
 })
 
 
+//mode édition
 
-const boutonObjets = document.querySelector(".objets")
-boutonObjets.addEventListener('click', () => {
-   
-    const projetObjets = projets.filter(function (projets){
-        return projets.categoryId === 1
+const introduction = document.getElementById("introduction")
+const portFolio = document.getElementById("portfolio")
+
+function createButtonEdit() {
+    const boutonEdition = document.createElement("button")
+    boutonEdition.innerText = "Edition";
+    boutonEdition.classList.add("filtres-boutton")
+}
+
+// boutonEdition.appendChild(portFolio)
+
+// afficher mode édition 
+
+function afficherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    // La popup est masquée par défaut (display:none), ajouter la classe "active"
+    // va changer son display et la rendre visible. 
+    popupBackground.classList.add("active")
+}
+
+/**
+ * Cette fonction cache la popup pour partager son score. 
+ */
+function cacherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    // La popup est masquée par défaut (display:none), supprimer la classe "active"
+    // va rétablir cet affichage par défaut. 
+    popupBackground.classList.remove("active")
+}
+/**
+ * Cette fonction initialise les écouteurs d'événements qui concernent 
+ * l'affichage de la popup. 
+ */
+function initAddEventListenerPopup() {
+    // On écoute le click sur le bouton "partager"
+    btnEdit = document.querySelector(".boutonEdition")
+    let popupBackground = document.querySelector(".popupBackground")
+    btnEdit.addEventListener("click", () => {
+        // Quand on a cliqué sur le bouton partagé, on affiche la popup
+        afficherPopup()
     })
-    mesProjets(projetObjets)
-})
 
-const boutonAppartements = document.querySelector(".appartements")
-boutonAppartements.addEventListener("click", () => {
-    
-    const projetAppartements = projets.filter(function (projets){
-        return projets.categoryId === 2
+    // On écoute le click sur la div "popupBackground"
+    popupBackground.addEventListener("click", (event) => {
+        // Si on a cliqué précisément sur la popupBackground 
+        // (et pas un autre élément qui se trouve dedant)
+        if (event.target === popupBackground) {
+            // Alors on cache la popup
+            cacherPopup()
+        }
     })
-    mesProjets(projetAppartements)
-})
-
-const boutonHotelRestaurant = document.querySelector(".hotel-restaurants")
-boutonHotelRestaurant.addEventListener("click", () => {
-    
-    const projetHotelRestaurant = projets.filter(projets => projets.categoryId === 3)
-    mesProjets(projetHotelRestaurant)
-})
+}
+initAddEventListenerPopup()
