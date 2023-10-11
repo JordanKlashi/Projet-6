9562546c8f8a802492809f0760be05d90d160abf
Verbreuk Jordan <jordan-010@hotmail.com>
2023-10-11 13:26:08 +0200

    comit
    comit

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/login.js
@@ -1,3 +1,5 @@
+const headerEdit = document.querySelector(".headerEdit")
+const btnEdit = document.querySelector(".boutonEdit")
 const loginMail = document.querySelector(".loginmail")
 
 const loginPassword = document.querySelector(".loginpassword")
@@ -6,10 +8,17 @@ const loginSubmit = document.querySelector(".loginsubmit")
 
 loginSubmit.addEventListener("click", (event) => {
     event.preventDefault()
-    if (loginPassword.value="S0phie"){
-        if (loginMail.value="sophie.bluel@test.tld"){
-            console.log("ça marche bien")
-        }}
-    
-    
+    if (loginPassword.value === "S0phie" && loginMail.value === "sophie.bluel@test.tld") {
+        console.log("ça marche bien")
+        window.location.href = "index.html";
+        btnEdit.classList.add("disabled")
+        headerEdit.classList.add("disabled")
+    } else {
+        alert("vous avez fais une erreur dans l'Email et/ou le Mot de passe")
+    }
+})
+
+const lostPassword = document.querySelector(".lostPassword")
+lostPassword.addEventListener("click", () => {
+    alert("Veuillez contactez votre Administrateur")
 })
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,242 +1,334 @@
 
-const apiWorks = "http://localhost:5678/api/works"
-const apiCategory = "http://localhost:5678/api/categories"
+const apiWorks = "http://localhost:5678/api/works";
+const apiCategory = "http://localhost:5678/api/categories";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
+
+let projets = [];
+let category = 0;
+let categoryData = [];
+let tryDelete = [];
+let categData;
+let categDataName;
 
 
-let projets = []
-let category = 0
-let categoryData = []
-// on récupére données de l'API Works
+
+// on récupère les données de l'API Works
 
 fetch(apiWorks)
     .then(reponse => {
         if (!reponse.ok) {
-            throw new Error(`Erreur HTTP! statut: ${reponse.status}`)
+            throw new Error(`Erreur HTTP! statut: ${reponse.status}`);
         }
         return reponse.json();
     })
     .then(data => {
-        projets = data
-        mesProjets()
-        modeEditionPhotos()
-
+        projets = data;
+        createButtons();
+        mesProjets();
+        modeEditionPhotos();
     })
     .catch(error => {
         console.error("Une erreur s'est produite lors de la récupération des données", error);
+    });
 
-    })
-
-
-// on récupére données de l'API Categories
+// on récupère les données de l'API Categories
 
 fetch(apiCategory)
     .then(reponse => {
         if (!reponse.ok) {
-            throw new Error(`Erreur HTTP! statut: ${reponse.status}`)
+            throw new Error(`Erreur HTTP! statut: ${reponse.status}`);
         }
         return reponse.json();
     })
     .then(data => {
-        categoryData = data
-        createButton()
-
-    })
+        categData = data
+        })
     .catch(error => {
         console.error("Une erreur s'est produite lors de la récupération des données", error);
+    });
 
+// on génère l'API Delete photos
+
+function deletePhoto(photoId) {
+    fetch(`http://localhost:5678/api/works/${photoId}`, {
+        method: "DELETE",
+        body: null,
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json; charset=UTF-8",
+        }
+        
     })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP! statut: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((json) => {
+            // La photo a été supprimée avec succès
+            deletePhoto()
+        })
+        .catch((error) => {
+            console.error("Une erreur s'est produite lors de la suppression de la photo", error);
+        });
+}
 
-// on génére l'API Delete
+// on récupére l'API Post photos
 
+function postPhoto() {
+    fetch(`http://localhost:5678/api/works/`), {
+        method: "POST",
+        body : {
+            "id": 0,
+            "title": "string",
+            "imageUrl": "string",
+            "categoryId": "string",
+            "userId": 1
+          },
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/json; charset=utf-8",
+        }
+        
+    }
+}
 
-// on génére les projets
-const gallery = document.querySelector(".gallery")
+// on génère les projets
+const gallery = document.querySelector(".gallery");
 
 function mesProjets() {
-    gallery.innerHTML = ""
+    gallery.innerHTML = "";
     for (let i = 0; i < projets.length; i++) {
         if (category === projets[i].categoryId || category === 0) {
             const article = projets[i];
-            const projet = document.createElement("figure")
-            const imageProjet = document.createElement("img")
+            const projet = document.createElement("figure");
+            const imageProjet = document.createElement("img");
             imageProjet.src = article.imageUrl;
             imageProjet.alt = article.title;
-            const textProjet = document.createElement("figcaption")
-            textProjet.innerText = article.title
+            const textProjet = document.createElement("figcaption");
+            textProjet.innerText = article.title;
 
-            // On rattache les balises a la section gallery
-
+            // On rattache les balises à la section gallery
             gallery.appendChild(projet);
             projet.appendChild(imageProjet);
             projet.appendChild(textProjet);
-
         }
     }
 }
 
-// on génére les boutons
-const divFiltres = document.querySelector(".filtres")
+// on génère les boutons
+const divFiltres = document.querySelector(".filtres");
 
-function createButton() {
-    for (let index = 0; index < categoryData.length; index++) {
-        const bouton = categoryData[index]
-        const boutonFiltres = document.createElement('button')
-        boutonFiltres.classList.add("filtres-boutton")
+function createButtons() {
+    const uniqueCategoryIds = [];
 
-        // on génére le nom des boutons 
+    for (let i = 0; i < projets.length; i++) {
+        const categoryId = projets[i].categoryId;
+        if (!uniqueCategoryIds.includes(categoryId)) {
+            uniqueCategoryIds.push(categoryId);
+        }
+    }
 
-        boutonFiltres.innerText = `${bouton.name}`
+    const divFiltres = document.querySelector(".filtres");
 
-        // on ajoute un EventListener sur la boucle des boutons
+    for (let index = 0; index < uniqueCategoryIds.length; index++) {
+        const categoryId = uniqueCategoryIds[index];
+        const categoryData = projets.find(p => p.categoryId === categoryId);
+        const boutonFiltres = document.createElement('button');
+        boutonFiltres.classList.add("filtres-boutton");
+
+        boutonFiltres.innerText = categoryData.category.name; // Utilisez le nom de la catégorie
 
         boutonFiltres.addEventListener("click", () => {
-            console.log("ça marche")
-            category = bouton.id
-            mesProjets()
-        })
+            category = categoryId; // Utilisez l'ID de catégorie
+            mesProjets();
+        });
 
-        // On rattache les balises a la section filtres
-
-        divFiltres.appendChild(boutonFiltres)
-        console.log(bouton)
+        divFiltres.appendChild(boutonFiltres);
     }
 }
 
-//gestion du boutton tous 
-
-const boutonTous = document.getElementById("tous")
+//gestion du bouton tous 
+const boutonTous = document.getElementById("tous");
 boutonTous.addEventListener("click", function () {
     category = 0;
-    mesProjets()
-})
+    mesProjets();
+});
 
+// relie le DOM
+const introduction = document.getElementById("introduction");
+const portFolio = document.getElementById("portfolio");
 
 //mode édition
-
-const introduction = document.getElementById("introduction")
-const portFolio = document.getElementById("portfolio")
-
-
-
-// boutonEdition.appendChild(portFolio)
-
 // afficher mode édition 
-
-let popupBackground = document.querySelector(".popupBackground")
-let cross = document.querySelector(".cross")
-const popup = document.querySelector(".popup")
-
+// on génère le popup
+let popupBackground = document.querySelector(".popupBackground");
+let cross = document.querySelector(".cross");
+const popup = document.querySelector(".popup");
+let arrowLeft = document.querySelector(".arrowLeft")
 function afficherPopup() {
-    // La popup est masquée par défaut (display:none), ajouter la classe "active"
-    // va changer son display et la rendre visible. 
-    popupBackground.classList.add("active")
-}
+    popupBackground.classList.add("active");
+    arrowLeft.classList.add("disabled")
 
-/**
- * Cette fonction cache la popup pour éditer. 
- */
-function cacherPopup() {
-    // La popup est masquée par défaut (display:none), supprimer la classe "active"
-    // va rétablir cet affichage par défaut. 
-    popupBackground.classList.remove("active")
 }
-/**
- * Cette fonction initialise les écouteurs d'événements qui concernent 
- * l'affichage de la popup. 
- */
+function cacherPopup() {
+    popupBackground.classList.remove("active");
+}
 function initAddEventListenerPopup() {
     // On écoute le click sur le bouton "Editer"
-    btnEdit = document.querySelector(".boutonEdition")
+    btnEdit = document.querySelector(".boutonEdition");
     btnEdit.addEventListener("click", () => {
-        // Quand on a cliqué sur le bouton Editer, on affiche la popup
-        afficherPopup()
-        modeEditionPhotos()
-    })
-
-    // On écoute le click sur la croix
+        afficherPopup();
+        modeEditionPhotos();
+    });
     cross.addEventListener("click", () => {
-        cacherPopup()
+        cacherPopup();
+    });
+    arrowLeft.addEventListener("click", () => {
+        modeEditionPhotos()
+        arrowLeft.classList.add("disabled")
     })
+    
 }
+initAddEventListenerPopup();
 
-initAddEventListenerPopup()
+// on génère les photos dans le mode edit 
+const galleryEdit = document.querySelector(".popup-galerie");
+const popupTitre = document.querySelector(".popup-titre");
 
-// on génére les photos dans le mode edit 
-
-const galleryEdit = document.querySelector(".popup-galerie")
-const popupTitre = document.querySelector(".popup-titre")
-
-
-
+// on met en place le popup modeEditionPhotos
 function modeEditionPhotos() {
-    galleryEdit.innerHTML = ""
+    galleryEdit.innerHTML = "";
+
     for (let i = 0; i < projets.length; i++) {
 
         const photos = projets[i]
 
-        const galleryEditPhotos = document.createElement("figure")
-        const galleryPhotos = document.createElement("img")
+        const galleryEditPhotos = document.createElement("figure");
+        const galleryPhotos = document.createElement("img");
 
-        const photosDelete = document.createElement("button")
-        photosDelete.classList.add("boutton-effacer")
-        photosDelete.innerText = "supprimer"
-
-
-        galleryPhotos.src = photos.imageUrl
-
-
-        galleryEdit.appendChild(galleryEditPhotos)
-        galleryEditPhotos.appendChild(galleryPhotos)
-        galleryEditPhotos.appendChild(photosDelete)
+        const buttonDeletePhotos = document.createElement("button");
+        buttonDeletePhotos.classList.add("boutton-effacer");
         
+
+        const iconTrash = document.createElement("i");
+        iconTrash.classList.add("fa-solid", "fa-trash"); // Ajoutez les classes Font Awesome
+
+        // Ajoutez l'icône au bouton
+        buttonDeletePhotos.appendChild(iconTrash);
+
+
+        buttonDeletePhotos.setAttribute("data-id", photos.id); // Stockez l'ID de la photo comme attribut de données
+        buttonDeletePhotos.addEventListener("click", () => {
+            console.log(`${photos.id}`)
+            
+        });
+
+        galleryPhotos.src = photos.imageUrl;
+
+        galleryEdit.appendChild(galleryEditPhotos);
+        galleryEditPhotos.appendChild(galleryPhotos);
+        galleryEditPhotos.appendChild(buttonDeletePhotos);
     }
-    popupTitre.innerText = "Galerie photos"
-    bouttonAddPhotos.classList.remove("disabled")
-   
+    popupTitre.innerText = "supprimer des photos";
+    bouttonAddPhotos.classList.remove("disabled");
 }
 
-const bouttonAddPhotos = document.createElement("button")
-    popup.appendChild(bouttonAddPhotos)
-    bouttonAddPhotos.classList.add("buttonAddPhotos")
-    bouttonAddPhotos.classList.add("boutonEdition")
-    bouttonAddPhotos.innerText = "Ajouter une photo"
+// on rajoute eventListener pour le clic sur le bouton 
+// on crée le bouton pour ajouter les photos 
+
+const bouttonAddPhotos = document.createElement("button");
+popup.appendChild(bouttonAddPhotos);
+bouttonAddPhotos.classList.add("buttonAddPhotos");
+bouttonAddPhotos.classList.add("boutonEdition");
+bouttonAddPhotos.innerText = "Ajouter une photo";
+
+//on génére la page d'ajout des photos
+
+function ajouterPhotos(categData) {
+    galleryEdit.innerHTML = ""
+    popupTitre.innerText = "Ajouter une photo"
+    arrowLeft.classList.remove("disabled")
+
+    const popupForm = document.createElement("form")
+    galleryEdit.appendChild(popupForm)
+    const labelImage = document.createElement("label"); // Créez un élément label
+    labelImage.setAttribute("for", "photoImage"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
+    labelImage.innerText = "Image"; // Texte du libellé
+    popupForm.appendChild(labelImage);
+
+
+    const photoImage = document.createElement("input")
+    popupForm.appendChild(photoImage)
+    photoImage.type = "file"
     
+    const labelTitre = document.createElement("label"); // Créez un élément label
+    labelTitre.setAttribute("for", "photoTitre"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
+    labelTitre.innerText = "Titre"; // Texte du libellé
+    popupForm.appendChild(labelTitre);
+    
+    const photoTitre = document.createElement("input")
+    popupForm.appendChild(photoTitre)
+    photoTitre.type = "text"
+    
+    const labelCategory = document.createElement("label"); // Créez un élément label
+    labelCategory.setAttribute("for", "photoCategory"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
+    labelCategory.innerText = "Catégorie"; // Texte du libellé
+    popupForm.appendChild(labelCategory);
+    
+    const selectCategory = document.createElement("select"); // Créez un élément select
+    selectCategory.name = "photoCategory"; // Nommez-le
+    popupForm.appendChild(selectCategory);
 
+    for (let i = 0 ; i < categData.length ; i++) {
+        const option = document.createElement("option"); 
+        option.value = categData[i].id; 
+        option.innerText = categData[i].name; 
+        selectCategory.appendChild(option); 
+    }
 
-    bouttonAddPhotos.addEventListener("click", () => {
+    const bouttonAddPhotosSubmit = document.createElement("button")
+    popupForm.appendChild(bouttonAddPhotosSubmit)
+    bouttonAddPhotosSubmit.classList.add("Trash")
+    bouttonAddPhotosSubmit.classList.add("boutonEdition")
+
+    bouttonAddPhotosSubmit.innerText = "Valider"
+
+    bouttonAddPhotosSubmit.addEventListener("click", (event) => {
+        event.preventDefault()
         
-        galleryEdit.innerHTML = ""
-        popupTitre.innerText = "Ajouter une photo"
-
-        const popupForm = document.createElement("form")
-        galleryEdit.appendChild(popupForm)
-        const labelImage = document.createElement("label"); // Créez un élément label
-        labelImage.setAttribute("for", "photoImage"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
-        labelImage.innerText = "Image"; // Texte du libellé
-        popupForm.appendChild(labelImage);
-
-
-        const photoImage = document.createElement("input")
-        popupForm.appendChild(photoImage)
-        photoImage.type = "file"
-        const labelTitre = document.createElement("label"); // Créez un élément label
-        labelTitre.setAttribute("for", "photoTitre"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
-        labelTitre.innerText = "Titre"; // Texte du libellé
-        popupForm.appendChild(labelTitre);
-        const photoTitre = document.createElement("input")
-        popupForm.appendChild(photoTitre)
-        photoTitre.type = "text"
-        const labelCategory = document.createElement("label"); // Créez un élément label
-        labelCategory.setAttribute("for", "photoCategory"); // Associez-le au champ d'entrée en utilisant l'attribut 'for'
-        labelCategory.innerText = "Catégorie"; // Texte du libellé
-        popupForm.appendChild(labelCategory);
-        const photoCategory = document.createElement("input")     
-        popupForm.appendChild(photoCategory)
-        photoCategory.type = "select"
-        const bouttonAddPhotosSubmit = document.createElement("button")
-        popupForm.appendChild(bouttonAddPhotosSubmit)
-        bouttonAddPhotosSubmit.classList.add("boutonEdition")
-        bouttonAddPhotosSubmit.innerText = "Valider Ajout"
-
-        bouttonAddPhotos.classList.add("disabled")
     })
+
+    bouttonAddPhotos.classList.add("disabled")
+}
+
+
+bouttonAddPhotos.addEventListener("click", () => {
+    ajouterPhotos(categData)
+})
+
+
+
+
+
+
+// action a modifier quand actualiser le login 
+// pris contact pour effectuer le test
+const headerEdit = document.querySelector(".headerEdit")
+
+const loginTest = document.querySelector(".logintest")
+loginTest.addEventListener("click", () => {
+btnEdit.classList.add("disabled")
+headerEdit.classList.add("disabled")
+})
+
+// même chose pour quand on se connecte
+// permet de protéger la possibilités d'Edit
+
+const logoutTest = document.querySelector(".logouttest")
+logoutTest.addEventListener("click", () => {
+    btnEdit.classList.remove("disabled")
+    headerEdit.classList.remove("disabled")
+})
+
